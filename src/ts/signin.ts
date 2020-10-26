de9162289e3e54e3942df3df3a9ab4bbc54037a4
Alex Sandri <me@alexsandri.com>
2020-10-26 14:31:35 +0100

    Show error messages in /signin

--- src/ts/signin.ts
+++ src/ts/signin.ts
@@ -2,6 +2,9 @@ import { Api } from "./classes/Api";
 
 const form = document.querySelector("form") as HTMLFormElement;
 
+const emailInput = form.querySelector("#email") as HTMLInputElement;
+const passwordInput = form.querySelector("#password") as HTMLInputElement;
+
 const submitButton = form.querySelector("button[type=submit]") as HTMLButtonElement;
 
 form.addEventListener("submit", async e =>
@@ -10,12 +13,19 @@ form.addEventListener("submit", async e =>
 
     submitButton.disabled = true;
 
-    const formData = new FormData(form);
+    [ emailInput, passwordInput ].forEach(element => element.parentElement?.querySelector(".error")?.remove());
 
     const response = await Api.Sessions.create({
-        email: (formData.get("email") as string).trim(),
-        password: formData.get("password") as string,
+        email: emailInput.value.trim(),
+        password: passwordInput.value,
     });
+
+    if (!response.result.valid)
+    {
+        emailInput.insertAdjacentElement("afterend", Api.Elements.error(response.errors.email.error));
+
+        passwordInput.insertAdjacentElement("afterend", Api.Elements.error(response.errors.password.error));
+    }
 
     console.log(response);
 
