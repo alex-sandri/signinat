be8b5aaa16a1bb4d22141f96bb2753664ae2acf6
Alex Sandri <me@alexsandri.com>
2020-10-27 16:03:54 +0100

    Reset form if it is valid

--- src/ts/signup.ts
+++ src/ts/signup.ts
@@ -52,8 +52,10 @@ form.addEventListener("submit", async e =>
 
         passwordInput.insertAdjacentElement("afterend", Api.Elements.error(response.errors.password.error));
     }
-
-    console.log(response);
+    else
+    {
+        form.reset();
+    }
 
     submitButton.disabled = false;
 });
