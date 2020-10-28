0752cb80c7decf1c13f127a8cfb174231ee15012
Alex Sandri <me@alexsandri.com>
2020-10-28 14:51:42 +0100

    Update

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,3 +4,5 @@ window.addEventListener("load", () => document.body.classList.add("loaded"));
 
 if (!Settings.exists("session") && location.pathname.startsWith("/account"))
     location.href = "/";
+
+document.documentElement.setAttribute("signed-in", Settings.exists("session").toString());
