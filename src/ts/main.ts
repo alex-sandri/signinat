9068cdcbb7590f7e95f7b97381685d1db495e4fc
Alex Sandri <me@alexsandri.com>
2020-10-28 18:34:55 +0100

    Update

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -6,6 +6,16 @@ window.addEventListener("load", () => document.body.classList.add("loaded"));
 if (!Settings.exists("session") && location.pathname.startsWith("/account"))
     location.href = "/";
 
+if (Settings.exists("session"))
+{
+    Api.Sessions.retrieve(Settings.get("session") as string).then(session =>
+    {
+        document.querySelectorAll<HTMLElement>(".first-name").forEach(element => element.innerText = session.user.firstName);
+        document.querySelectorAll<HTMLElement>(".last-name").forEach(element => element.innerText = session.user.lastName);
+        document.querySelectorAll<HTMLElement>(".email").forEach(element => element.innerText = session.user.email);
+    });
+}
+
 document.documentElement.setAttribute("signed-in", Settings.exists("session").toString());
 
 const signOutButton = document.querySelector("#sign-out") as HTMLButtonElement;
--- src/ts/settings.ts
+++
@@ -1,9 +0,0 @@
-import { Api } from "./classes/Api";
-import { Settings } from "./classes/Settings";
-
-Api.Sessions.retrieve(Settings.get("session") as string).then(session =>
-{
-    (document.querySelector("#first-name .value") as HTMLElement).innerText = session.user.firstName;
-    (document.querySelector("#last-name .value") as HTMLElement).innerText = session.user.lastName;
-    (document.querySelector("#email .value") as HTMLElement).innerText = session.user.email;
-});
