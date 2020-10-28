fb9093d4cd2f9c483bf7c5762f5885ad39db2e76
Alex Sandri <me@alexsandri.com>
2020-10-28 15:04:48 +0100

    Show user info in settings

--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -3,5 +3,7 @@ import { Settings } from "./classes/Settings";
 
 Api.Sessions.retrieve(Settings.get("session") as string).then(session =>
 {
-    console.log(session);
+    (document.querySelector("#first-name .value") as HTMLElement).innerText = session.user.firstName;
+    (document.querySelector("#last-name .value") as HTMLElement).innerText = session.user.lastName;
+    (document.querySelector("#email .value") as HTMLElement).innerText = session.user.email;
 });
