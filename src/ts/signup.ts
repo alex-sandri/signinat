655674c666e0062c00fd92d8dd13513f9027b801
Alex Sandri <me@alexsandri.com>
2020-11-01 18:39:25 +0100

    Remove app form

--- src/ts/signup.ts
+++ src/ts/signup.ts
@@ -2,34 +2,16 @@ import * as zxcvbn from "zxcvbn";
 
 import { Api } from "./classes/Api";
 
-const userForm = document.querySelector("form[data-for-account-type=user]") as HTMLFormElement;
-const appForm = document.querySelector("form[data-for-account-type=app]") as HTMLFormElement;
+const form = document.querySelector("form") as HTMLFormElement;
 
-const accountTypeRadioButtons = document.querySelectorAll<HTMLInputElement>("#account-type-choice input[type=radio]");
+const firstNameInput = form.querySelector("#first-name") as HTMLInputElement;
+const lastNameInput = form.querySelector("#last-name") as HTMLInputElement;
+const emailInput = form.querySelector("#email") as HTMLInputElement;
+const passwordInput = form.querySelector("#password") as HTMLInputElement;
 
-const firstNameInput = userForm.querySelector("#first-name") as HTMLInputElement;
-const lastNameInput = userForm.querySelector("#last-name") as HTMLInputElement;
-const emailInput = userForm.querySelector("#email") as HTMLInputElement;
-const passwordInput = userForm.querySelector("#password") as HTMLInputElement;
+const submitButton = form.querySelector("button[type=submit]") as HTMLButtonElement;
 
-const appNameInput = appForm.querySelector("#app-name") as HTMLInputElement;
-const appUrlInput = appForm.querySelector("#app-url") as HTMLInputElement;
-const appEmailInput = appForm.querySelector("#app-email") as HTMLInputElement;
-const appPasswordInput = appForm.querySelector("#app-password") as HTMLInputElement;
-
-const userFormSubmitButton = userForm.querySelector("button[type=submit]") as HTMLButtonElement;
-const appFormSubmitButton = appForm.querySelector("button[type=submit]") as HTMLButtonElement;
-
-accountTypeRadioButtons.forEach(radioButton => radioButton.addEventListener("change", e =>
-{
-    const accountType = (e.target as HTMLInputElement).getAttribute("data-account-type") as string;
-
-    document.querySelectorAll<HTMLElement>(`[data-for-account-type]`).forEach(element => element.style.display = "none");
-
-    (document.querySelector(`[data-for-account-type=${accountType}]`) as HTMLElement).style.display = "block";
-}));
-
-[ passwordInput, appPasswordInput ].forEach(element => element.addEventListener("input", e =>
+passwordInput.addEventListener("input", e =>
 {
     const zxcvbnResult = zxcvbn(passwordInput.value);
 
@@ -41,13 +23,13 @@ accountTypeRadioButtons.forEach(radioButton => radioButton.addEventListener("cha
     {
         passwordInput.insertAdjacentElement("afterend", Api.Elements.suggestion(suggestion));
     });
-}));
+});
 
-userForm.addEventListener("submit", async e =>
+form.addEventListener("submit", async e =>
 {
     e.preventDefault();
 
-    userFormSubmitButton.disabled = true;
+    submitButton.disabled = true;
 
     [ firstNameInput, lastNameInput, emailInput, passwordInput ].forEach(element => element.parentElement?.querySelector(".error")?.remove());
 
@@ -72,41 +54,8 @@ userForm.addEventListener("submit", async e =>
     }
     else
     {
-        userForm.reset();
+        form.reset();
     }
 
-    userFormSubmitButton.disabled = false;
-});
-
-appForm.addEventListener("submit", async e =>
-{
-    e.preventDefault();
-
-    appFormSubmitButton.disabled = true;
-
-    [ appNameInput, appUrlInput, appEmailInput, appPasswordInput ].forEach(element => element.parentElement?.querySelector(".error")?.remove());
-
-    const response = await Api.Apps.create({
-        name: appNameInput.value.trim(),
-        url: appUrlInput.value.trim(),
-        email: appEmailInput.value.trim(),
-        password: appPasswordInput.value,
-    });
-
-    if (!response.result.valid)
-    {
-        appNameInput.insertAdjacentElement("afterend", Api.Elements.error(response.errors.name.error));
-
-        appUrlInput.insertAdjacentElement("afterend", Api.Elements.error(response.errors.url.error));
-
-        appEmailInput.insertAdjacentElement("afterend", Api.Elements.error(response.errors.email.error));
-
-        appPasswordInput.insertAdjacentElement("afterend", Api.Elements.error(response.errors.password.error));
-    }
-    else
-    {
-        appForm.reset();
-    }
-
-    appFormSubmitButton.disabled = false;
+    submitButton.disabled = false;
 });
